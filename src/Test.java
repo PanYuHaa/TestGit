ba8386d335bb17ff0cf9a3fae8c3027d93706df1
PanYuHan <1352164869@qq.com>
2019-12-27 21:18:24 +0800

    第一次上传

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import java.security.PublicKey;
+
+/**
+ * @author PanYuHan
+ * @date 2019/12/27
+ **/
+public class Test {
+
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.show();
+    }
+
+    public static void show(){
+        System.out.println("Test");
+    }
+}
